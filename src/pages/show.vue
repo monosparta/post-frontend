85acedb45a540500cbd600c7c8e2e79028152a6b
yang <yang@gmail.com>
2022-10-14 13:56:36 +0800

    feat: cookie change localstorage

--- src/pages/show.vue
+++ src/pages/show.vue
@@ -1,20 +1,30 @@
 <script setup lang="ts">
 const showPost = useShowPostStore()
 
-const cookieTaken = (sKey: string) => {
-  const aKeys = document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[-.+*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$/"), "$1")
-  return aKeys
-}
-
-const tokenTitle = cookieTaken('title')
-const tokenContent = cookieTaken('content')
-const tokenUserId = cookieTaken('userId')
-const tokenDateTime = cookieTaken('dateTime')
+// const cookieTaken = (sKey: string) => {
+//   const aKeys = document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[-.+*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$/"), "$1")
+//   return aKeys
+// }
+// const tokenTitle = cookieTaken('title')
+// const tokenContent = cookieTaken('content')
+// const tokenUserId = cookieTaken('userId')
+// const tokenDateTime = cookieTaken('dateTime')
+// console.log(tokenTitle);
+// console.log(tokenContent);
+// console.log(tokenUserId);
+// console.log(tokenDateTime);
+const tokenId = localStorage.getItem("id")
+const tokenTitle = localStorage.getItem("title")
+const tokenContent = localStorage.getItem("content")
+const tokenCreateAt = localStorage.getItem("createAt")
+const tokenUserId = localStorage.getItem("userId")
+const tokenUseName = localStorage.getItem("useName")
+console.log(tokenId);
 console.log(tokenTitle);
 console.log(tokenContent);
+console.log(tokenCreateAt);
 console.log(tokenUserId);
-console.log(tokenDateTime);
-
+console.log(tokenUseName);
 showPost.createNotification({
   type1: 'content',
   type2: 'personal',
