f8ddbf1feee81624d1136eaa6758245c27065c00
Shin <Noiecat128@gmail.com>
2022-10-16 01:18:51 +0800

    feat: usrPostStore add getPost and clearPost

--- src/store/post.ts
+++ src/store/post.ts
@@ -1,6 +1,19 @@
 import { acceptHMRUpdate, defineStore } from 'pinia'
 import axios from 'axios'
 const user = useUserStore()
+const emptyPost = {
+    data: {
+        id: '',
+        title: '',
+        content: '',
+        created_at: '',
+        updated_at: '',
+        user: {
+            id: '',
+            name: '',
+        },
+    },
+}
 export const usePostStore = defineStore('post', () => {
     const posts = reactive({
         value: {
@@ -21,6 +34,8 @@ export const usePostStore = defineStore('post', () => {
     })
 
     const list = computed(() => { return posts.value.data })
+    const post = reactive({ ...emptyPost })
+    const info = computed(() => { return post.data })
     const getPosts = async () => {
         const logged = await user.checkToken()
         if (!logged)
@@ -35,10 +50,30 @@ export const usePostStore = defineStore('post', () => {
             console.log(error)
         }
     }
-
+    const getPost = async (id: string) => {
+        const logged = await user.checkToken()
+        if (!logged)
+            return
+        try {
+            const res = await axios.get(
+                `${import.meta.env.VITE_APP_API_URL}/api/post/${id}`,
+            )
+            post.data = res.data
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+    const clearPost = () => {
+        post.data = emptyPost.data
+    }
     return {
         list,
+        post,
+        info,
         getPosts,
+        getPost,
+        clearPost,
     }
 })
 
