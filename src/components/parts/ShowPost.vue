fc2def3d5ee9c687bd977808fd130004fbb2ac9e
Shin <Noiecat128@gmail.com>
2022-10-16 01:16:35 +0800

    fix: remove unused variables

--- src/components/parts/ShowPost.vue
+++ src/components/parts/ShowPost.vue
@@ -9,8 +9,7 @@ const cookieTaken = (sKey: string) => {
 const tokenUserId = cookieTaken('userId')
 
 const props = defineProps({
-  type1: String,
-  type2: String,
+  type: String,
   id: String,
   title: String,
   content: String,
@@ -29,7 +28,7 @@ const props = defineProps({
           <h2 id="notes-title" class="col-start-1 col-end-5 text-2xl font-semibold font-Inter text-gray-900">
             {{props.title}}
           </h2>
-          <div v-if="props.type2==='personal' || props.userId===tokenUserId"
+          <div v-if=" props.userId===tokenUserId"
             class="col-end-7 col-span-2 relative flex justify-end whitespace-nowrap  text-right text-sm font-medium">
             <button class="flex justify-center  text-gray-400 hover:text-gray-900" @click="">
               <TrashIcon class="h-6 w-6 mx-2" aria-hidden="true" /><span class="sr-only">{{ props.id
@@ -46,17 +45,17 @@ const props = defineProps({
           <ul role="list">
             <li>
               <div class="text-base font-normal font-Inter text-gray-400">
-                {{userName}} - {{props.dateTime}}
+                {{props.userName}} - {{props.dateTime}}
               </div>
             </li>
             <li class="pt-2 pb-4">
-              <div v-if="props.type1==='content'" class="space-x-3 text-xl font-light font-Inter whitespace-pre-line">
+              <div v-if="props.type==='content'" class="space-x-3 text-xl font-light font-Inter whitespace-pre-line">
                 {{props.content}}
               </div>
-              <div v-else-if="props.type1==='frontPage'" class="space-x-3 text-xl font-light font-Inter">
+              <div v-else-if="props.type==='frontPage'" class="space-x-3 text-xl font-light font-Inter">
                 <p class="line-clamp-2">{{props.content}}</p>
               </div>
-              <div v-if="props.type1==='frontPage'" class="grid grid-cols-5 justify-items-end grep-4">
+              <div v-if="props.type==='frontPage'" class="grid grid-cols-5 justify-items-end grep-4">
                 <button
                   class="col-end-6 col-span-1 mx-0 my-0  text-base font-normal text-indigo-700 hover:text-indigo-400"
                   @click="$router.push(`/show/${props.id}`)">
