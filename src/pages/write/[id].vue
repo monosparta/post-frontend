ff0f57f45905b648c0bba4c3cb59aef054b3e3ce
Shin <Noiecat128@gmail.com>
2022-10-18 21:54:05 +0800

    fix: Optimize button code

--- src/pages/write/[id].vue
+++ src/pages/write/[id].vue
@@ -1,31 +1,31 @@
 <script setup lang="ts">
-import axios from 'axios'
-const modal = useModalStore()
+const user = useUserStore()
 const route = useRoute()
 const post = usePostStore()
+const modal = useModalStore()
 const titleInput = ref('')
-const textInput = ref('')
+const contentInput = ref('')
 let modalType = ref('')
 const postId = route.params.id as string
 
-const check = async () => {
-  if (titleInput.value == '' || textInput.value == '') {
+const checkPostEmpty = async () => {
+  if (titleInput.value == '' || contentInput.value == '') {
     console.log('空的！');
 
     modalType.value = 'information'
     modal.createNotification({
-      type: 'warming',
+      type: 'warning',
       text: '標題或內文欄位不可以空白喔！',
-      dateTime: '',
+      postId: '',
     })
   } else {
     console.log('有資料');
 
     modalType.value = 'check'
     modal.createNotification({
-      type: 'update',
+      type: 'add',
       text: '確定修改該篇文章？',
-      dateTime: '',
+      postId: '',
     })
   }
 }
@@ -33,57 +33,35 @@ const check = async () => {
 const setData = async () => {
   await post.getPost(postId)
   titleInput.value = post.info.title
-  textInput.value = post.info.content
+  contentInput.value = post.info.content
 }
 
 onMounted(async () => {
   await setData()
 })
 
-
-const updatePost = async (data: {
-  title: string
-  content: string
-  postId: string
-}) => {
-  const result = await axios.post(
-    `${import.meta.env.VITE_APP_API_URL}/api/post`, data,
-  )
-  console.log(result.data)
-  return result.data
-}
-
 const confirmPost = async () => {
   modalType.value = 'loading'
-  const title = titleInput.value as string;
-  const content = textInput.value;
-  // const user_id = user.userData.id
+  const title = titleInput.value
+  const content = contentInput.value
+  const user_id = user.userData.id
+  await post.createPost({ title, content, user_id })
+  console.log(post.infoCheckStatus);
 
-  const getResult = await updatePost({ title, content, postId })
-
-  if (getResult) {
-    if (getResult.message === 'incorrect format') {
-      modalType.value = 'information'
-      modal.createNotification({
-        type: 'warming',
-        text: 'incorrent format',
-        dateTime: '',
-      })
-    } else {
-      localStorage.setItem('id', getResult.id)
-      localStorage.setItem('title', getResult.title)
-      localStorage.setItem('content', getResult.content)
-      localStorage.setItem('createdAt', getResult.created_at.substring(0, 19).replace("T", " "))
-      localStorage.setItem('userId', getResult.user.id)
-      localStorage.setItem('userName', getResult.user.name)
-      modalType.value = ''
-      modalType.value = 'information'
-      modal.createNotification({
-        type: 'update',
-        text: '修改',
-        dateTime: getResult.created_at.substring(0, 19).replace("T", " "),
-      })
-    }
+  if (post.infoCheckStatus === 200) {
+    modalType.value = 'information'
+    modal.createNotification({
+      type: 'add',
+      text: '已修改該篇文章！',
+      postId: post.info.id,
+    })
+  } else {
+    modalType.value = 'information'
+    modal.createNotification({
+      type: 'warning',
+      text: `修改失敗！ ${post.infoCheckStatus}`,
+      postId: '',
+    })
   }
 }
 </script>
@@ -91,26 +69,25 @@ const confirmPost = async () => {
 <template>
   <div class="mt-4 mx-16 mb-4">
     <div class="flex flex-col">
-      <BulletinSectionHeaderForPost :title="'編輯文章'" :needButton="false" />
+      <BulletinSectionHeaderForPost title="編輯文章" :needButton="false" />
       <main class="auto-cols-min">
         <div class="mt-4 h-10">
-          <input id="title" name="title" type="txt" autocomplete="title" placeholder="Title write here..."
+          <input id="title" name="title" type="txt" autocomplete="title" placeholder="請輸入文章標題"
             class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
             v-model="titleInput" />
         </div>
         <div class="mt-4">
           <textarea rows="4" name="comment" id="comment"
-            class="block w-full rounded-md border-gray-300 shadow-sm  focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-            style="height:576px" placeholder="Txt write here..." v-model="textInput"></textarea>
+            class="h-[576px] block w-full rounded-md border-gray-300 shadow-sm  focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+            placeholder="請輸入文章內容" v-model="contentInput"></textarea>
         </div>
-        <WriteButton :titleHeader="'編輯文章'" @check="check()" />
+        <WriteButton buttonShow="修改文章" @click="checkPostEmpty()" />
       </main>
       <Check v-if="modalType==='check' && modal.notificationStatus===true" :text="modal.notification.text"
-        :dateTime="modal.notification.dateTime" :type="modal.notification.type" @click="modal.closeNotification"
-        @confirm="confirmPost()" />
+        :type="modal.notification.type" @click="modal.closeNotification" @confirm="confirmPost()" />
       <LoadingModal v-if="modalType === 'loading'" />
-      <Information v-if="modalType==='information' && modal.notificationStatus===true" :title="modal.notification.text"
-        :dateTime="modal.notification.dateTime" :type="modal.notification.type" @click="modal.closeNotification" />
+      <Information v-if="modalType==='information' && modal.notificationStatus===true" :text="modal.notification.text"
+        :postId="modal.notification.postId" :type="modal.notification.type" @click="modal.closeNotification" />
     </div>
   </div>
 </template>
--- src/pages/write/index.vue
+++ src/pages/write/index.vue
@@ -22,7 +22,7 @@ const checkPostEmpty = async () => {
     modalType.value = 'check'
     modal.createNotification({
       type: 'add',
-      text: '確定新增 該篇文章？',
+      text: '確定新增該篇文章？',
       postId: '',
     })
   }
@@ -34,9 +34,7 @@ const confirmPost = async () => {
   const content = contentInput.value
   const user_id = user.userData.id
   await post.createPost({ title, content, user_id })
-  console.log(post.infoCheckStatus);
 
-  // TODO:調整判斷 並將localStorage更換掉 Ｖ
   if (post.infoCheckStatus === 201) {
     modalType.value = 'information'
     modal.createNotification({
@@ -61,16 +59,16 @@ const confirmPost = async () => {
       <BulletinSectionHeaderForPost title="新增文章" :needButton="false" />
       <main class="auto-cols-min">
         <div class="mt-4 h-10">
-          <input id="title" name="title" type="txt" autocomplete="title" placeholder="Title write here"
+          <input id="title" name="title" type="txt" autocomplete="title" placeholder="請輸入文章標題"
             class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
             v-model="titleInput" />
         </div>
         <div class="mt-4">
           <textarea rows="4" name="comment" id="comment"
             class="h-[576px] block w-full rounded-md border-gray-300 shadow-sm  focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-            placeholder="TXT write here" v-model="contentInput"></textarea>
+            placeholder="請輸入文章內容" v-model="contentInput"></textarea>
         </div>
-        <WriteButton titleHeader="新增文章" @check="checkPostEmpty()" />
+        <WriteButton buttonShow="新增文章" @click="checkPostEmpty()" />
       </main>
       <Check v-if="modalType==='check' && modal.notificationStatus===true" :text="modal.notification.text"
         :type="modal.notification.type" @click="modal.closeNotification" @confirm="confirmPost()" />
